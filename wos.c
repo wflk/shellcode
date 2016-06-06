c5a6f8f71058dc36a7547720d4dd73e8351efc96
Odzhan <odzhan@protonmail.com>
2016-06-06 22:40:05 +0100

    wos

--- wos.c
+++ wos.c
@@ -86,79 +86,82 @@ void xstrerror (char *fmt, ...)
 
 #ifndef TEST
 
-#define w_SIZE 122
+
+#define w_SIZE 124
 
 char w[]= {
   /* 0000 */ "\x57"             /* push edi           */
-  /* 0001 */ "\x51"             /* push ecx           */
-  /* 0002 */ "\x5f"             /* pop edi            */
-  /* 0003 */ "\x54"             /* push esp           */
-  /* 0004 */ "\x58"             /* pop eax            */
-  /* 0005 */ "\xc1\xe8\x18"     /* shr eax, 0x18      */
-  /* 0008 */ "\x74\x02"         /* jz 0xc             */
-  /* 000A */ "\x5f"             /* pop edi            */
-  /* 000B */ "\x57"             /* push edi           */
-  /* 000C */ "\x31\xc0"         /* xor eax, eax       */
-  /* 000E */ "\x48"             /* dec eax            */
-  /* 000F */ "\xf7\xd8"         /* neg eax            */
-  /* 0011 */ "\x74\x04"         /* jz 0x17            */
-  /* 0013 */ "\x8b\x7c\x24\x08" /* mov edi, [esp+0x8] */
-  /* 0017 */ "\xab"             /* stosd              */
-  /* 0018 */ "\x8d\x48\xff"     /* lea ecx, [eax-0x1] */
-  /* 001B */ "\x54"             /* push esp           */
-  /* 001C */ "\x58"             /* pop eax            */
-  /* 001D */ "\xc1\xe8\x18"     /* shr eax, 0x18      */
-  /* 0020 */ "\x0f\x94\xd0"     /* setz al            */
-  /* 0023 */ "\xab"             /* stosd              */
-  /* 0024 */ "\x66\x8c\xc8"     /* mov ax, cs         */
-  /* 0027 */ "\x66\xab"         /* stosw              */
-  /* 0029 */ "\x66\x8c\xd8"     /* mov ax, ds         */
-  /* 002C */ "\x66\xab"         /* stosw              */
-  /* 002E */ "\x66\x8c\xc0"     /* mov ax, es         */
-  /* 0031 */ "\x66\xab"         /* stosw              */
-  /* 0033 */ "\x66\x8c\xe0"     /* mov ax, fs         */
-  /* 0036 */ "\x66\xab"         /* stosw              */
-  /* 0038 */ "\x66\x8c\xe8"     /* mov ax, gs         */
-  /* 003B */ "\x66\xab"         /* stosw              */
-  /* 003D */ "\x66\x8c\xd0"     /* mov ax, ss         */
-  /* 0040 */ "\x66\xab"         /* stosw              */
-  /* 0042 */ "\x54"             /* push esp           */
-  /* 0043 */ "\x58"             /* pop eax            */
-  /* 0044 */ "\xab"             /* stosd              */
-  /* 0045 */ "\xe3\x1c"         /* jecxz 0x63         */
-  /* 0047 */ "\x48"             /* dec eax            */
-  /* 0048 */ "\xc1\xc8\x20"     /* ror eax, 0x20      */
-  /* 004B */ "\xab"             /* stosd              */
-  /* 004C */ "\x51"             /* push ecx           */
-  /* 004D */ "\x57"             /* push edi           */
-  /* 004E */ "\x6a\xff"         /* push 0xffffffff    */
-  /* 0050 */ "\x5f"             /* pop edi            */
-  /* 0051 */ "\x6a\x06"         /* push 0x6           */
-  /* 0053 */ "\x58"             /* pop eax            */
-  /* 0054 */ "\x0f\x05"         /* syscall            */
-  /* 0056 */ "\x5f"             /* pop edi            */
-  /* 0057 */ "\x59"             /* pop ecx            */
-  /* 0058 */ "\xab"             /* stosd              */
-  /* 0059 */ "\x48"             /* dec eax            */
-  /* 005A */ "\xc1\xc8\x20"     /* ror eax, 0x20      */
-  /* 005D */ "\xab"             /* stosd              */
-  /* 005E */ "\x5f"             /* pop edi            */
-  /* 005F */ "\x6a\x01"         /* push 0x1           */
-  /* 0061 */ "\x58"             /* pop eax            */
-  /* 0062 */ "\xc3"             /* ret                */
-  /* 0063 */ "\x66\x8c\xe9"     /* mov cx, gs         */
-  /* 0066 */ "\xe3\xf6"         /* jecxz 0x5e         */
-  /* 0068 */ "\x54"             /* push esp           */
-  /* 0069 */ "\x58"             /* pop eax            */
-  /* 006A */ "\xc1\xe8\x18"     /* shr eax, 0x18      */
-  /* 006D */ "\x74\xef"         /* jz 0x5e            */
-  /* 006F */ "\x6a\xff"         /* push 0xffffffff    */
-  /* 0071 */ "\x5b"             /* pop ebx            */
-  /* 0072 */ "\x6a\x06"         /* push 0x6           */
-  /* 0074 */ "\x58"             /* pop eax            */
-  /* 0075 */ "\xcd\x80"         /* int 0x80           */
-  /* 0077 */ "\xab"             /* stosd              */
-  /* 0078 */ "\xeb\xe4"         /* jmp 0x5e           */
+  /* 0001 */ "\x53"             /* push ebx           */
+  /* 0002 */ "\x51"             /* push ecx           */
+  /* 0003 */ "\x5f"             /* pop edi            */
+  /* 0004 */ "\x54"             /* push esp           */
+  /* 0005 */ "\x58"             /* pop eax            */
+  /* 0006 */ "\xc1\xe8\x18"     /* shr eax, 0x18      */
+  /* 0009 */ "\x74\x02"         /* jz 0xd             */
+  /* 000B */ "\x5f"             /* pop edi            */
+  /* 000C */ "\x57"             /* push edi           */
+  /* 000D */ "\x31\xc0"         /* xor eax, eax       */
+  /* 000F */ "\x48"             /* dec eax            */
+  /* 0010 */ "\xf7\xd8"         /* neg eax            */
+  /* 0012 */ "\x74\x04"         /* jz 0x18            */
+  /* 0014 */ "\x8b\x7c\x24\x0c" /* mov edi, [esp+0xc] */
+  /* 0018 */ "\xab"             /* stosd              */
+  /* 0019 */ "\x8d\x48\xff"     /* lea ecx, [eax-0x1] */
+  /* 001C */ "\x54"             /* push esp           */
+  /* 001D */ "\x58"             /* pop eax            */
+  /* 001E */ "\xc1\xe8\x18"     /* shr eax, 0x18      */
+  /* 0021 */ "\x0f\x94\xd0"     /* setz al            */
+  /* 0024 */ "\xab"             /* stosd              */
+  /* 0025 */ "\x66\x8c\xc8"     /* mov ax, cs         */
+  /* 0028 */ "\x66\xab"         /* stosw              */
+  /* 002A */ "\x66\x8c\xd8"     /* mov ax, ds         */
+  /* 002D */ "\x66\xab"         /* stosw              */
+  /* 002F */ "\x66\x8c\xc0"     /* mov ax, es         */
+  /* 0032 */ "\x66\xab"         /* stosw              */
+  /* 0034 */ "\x66\x8c\xe0"     /* mov ax, fs         */
+  /* 0037 */ "\x66\xab"         /* stosw              */
+  /* 0039 */ "\x66\x8c\xe8"     /* mov ax, gs         */
+  /* 003C */ "\x66\xab"         /* stosw              */
+  /* 003E */ "\x66\x8c\xd0"     /* mov ax, ss         */
+  /* 0041 */ "\x66\xab"         /* stosw              */
+  /* 0043 */ "\x54"             /* push esp           */
+  /* 0044 */ "\x58"             /* pop eax            */
+  /* 0045 */ "\xab"             /* stosd              */
+  /* 0046 */ "\xe3\x1d"         /* jecxz 0x65         */
+  /* 0048 */ "\x48"             /* dec eax            */
+  /* 0049 */ "\xc1\xc8\x20"     /* ror eax, 0x20      */
+  /* 004C */ "\xab"             /* stosd              */
+  /* 004D */ "\x51"             /* push ecx           */
+  /* 004E */ "\x57"             /* push edi           */
+  /* 004F */ "\x6a\xff"         /* push 0xffffffff    */
+  /* 0051 */ "\x5f"             /* pop edi            */
+  /* 0052 */ "\x6a\x06"         /* push 0x6           */
+  /* 0054 */ "\x58"             /* pop eax            */
+  /* 0055 */ "\x0f\x05"         /* syscall            */
+  /* 0057 */ "\x5f"             /* pop edi            */
+  /* 0058 */ "\x59"             /* pop ecx            */
+  /* 0059 */ "\xab"             /* stosd              */
+  /* 005A */ "\x48"             /* dec eax            */
+  /* 005B */ "\xc1\xc8\x20"     /* ror eax, 0x20      */
+  /* 005E */ "\xab"             /* stosd              */
+  /* 005F */ "\x5b"             /* pop ebx            */
+  /* 0060 */ "\x5f"             /* pop edi            */
+  /* 0061 */ "\x6a\x01"         /* push 0x1           */
+  /* 0063 */ "\x58"             /* pop eax            */
+  /* 0064 */ "\xc3"             /* ret                */
+  /* 0065 */ "\x66\x8c\xe9"     /* mov cx, gs         */
+  /* 0068 */ "\xe3\xf5"         /* jecxz 0x5f         */
+  /* 006A */ "\x54"             /* push esp           */
+  /* 006B */ "\x58"             /* pop eax            */
+  /* 006C */ "\xc1\xe8\x18"     /* shr eax, 0x18      */
+  /* 006F */ "\x74\xee"         /* jz 0x5f            */
+  /* 0071 */ "\x6a\xff"         /* push 0xffffffff    */
+  /* 0073 */ "\x5b"             /* pop ebx            */
+  /* 0074 */ "\x6a\x06"         /* push 0x6           */
+  /* 0076 */ "\x58"             /* pop eax            */
+  /* 0077 */ "\xcd\x80"         /* int 0x80           */
+  /* 0079 */ "\xab"             /* stosd              */
+  /* 007A */ "\xeb\xe3"         /* jmp 0x5f           */
 };
 
 typedef void (*get_ctx_t)(proc_ctx*);
