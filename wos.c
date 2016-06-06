e844791ff594189ba04e358be2baf74777705bbe
Odzhan <odzhan@protonmail.com>
2016-06-06 18:23:19 +0100

    w

--- wos.c
+++ wos.c
@@ -1,4 +1,7 @@
 
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,14 +26,18 @@ typedef struct _proc_ctx_t {
 
 void get_ctx(proc_ctx *);
 
-void main(void) {
+int main(void) {
   proc_ctx pc;
   
-  printf ("\nsizeof(void*) = %u", sizeof(void*));
+  setbuf(stdout, NULL);
+  
+  printf ("\nsizeof(void*) = %u\nsizeof(uint32_t) = %u\n", 
+    sizeof(void*), sizeof(uint32_t));
   
   memset(&pc, 0, sizeof(pc));
   get_ctx(&pc);
       
   printf ("\n%s %i-bit cs=0x%02X ds=0x%02X es=0x%02X fs=0x%02X gs=0x%02X ss=0x%02X sp=%p sys_close error = %p\n",
     pc.win ? "Windows" : "NIX", pc.emu ? 32 : 64, pc.cs, pc.ds, pc.es, pc.fs, pc.gs, pc.ss, pc.sp, pc.sc);
+  return 0;
 }
